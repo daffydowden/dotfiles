e659b81fc25cb4a33658991abbb61fd4131812b3
Richard Dowden <rd@richarddowden.com>
2015-08-26 22:30:34 +0100

    Add fish shell config

---
+++ config/fish/config.fish
@@ -0,0 +1,22 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+#set fish_theme robbyrussell
+set fish_theme bobthefish
+
+set -g default_user richarddowden
+
+# All built-in plugins can be found at ~/.oh-my-fish/plugins/
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Enable plugins by adding their name separated by a space to the line below.
+set fish_plugins theme rbenv brew node tab
+
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Add local bin to path
+set PATH $PATH ~/bin
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
