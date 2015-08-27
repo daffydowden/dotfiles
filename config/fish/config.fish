fb61421620a34f82e0abfb3285f27fc7298de021
Richard Dowden <rd@richarddowden.com>
2015-08-27 11:49:10 +0100

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,9 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+set -g OMF_PATH $HOME/.local/share/omf
+set -g OMF_CUSTOM $HOME/projects/dotfiles/oh-my-fish
+set -g OMF_CONFIG $HOME/.config/omf
+source $OMF_PATH/init.fish
 
 # Theme
-#set fish_theme robbyrussell
 set fish_theme bobthefish
 
 set -g default_user richarddowden
@@ -10,7 +11,7 @@ set -g default_user richarddowden
 # All built-in plugins can be found at ~/.oh-my-fish/plugins/
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme rbenv brew node tab
+# set fish_plugins theme rbenv brew node tab
 
 # Path to your custom folder (default path is ~/.oh-my-fish/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
