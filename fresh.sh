ed90d2409898b2eb03c7074145d5a7a0c0658d05
Richard Dowden <rd@richarddowden.com>
2016-01-12 20:20:21 +0000

    Split brew file out into separate parts depending on purpose

--- fresh.sh
+++ fresh.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo -e "\n\nHomebrew\n"
+
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
@@ -9,6 +11,10 @@ fi
 
 # Update homebrew recipes
 brew update
+
+#------------------------------------------------------------------------------
+
+echo -e "\n\ncoreutils\n"
 
 # Install GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
@@ -27,10 +33,36 @@ brew install homebrew/dupes/grep
 $PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
 
 binaries=(
-  node
   tree
   ack
   git
 )
 
 brew cleanup
+
+#------------------------------------------------------------------------------
+
+echo -e "\n\nhomebrew-bundle\n"
+
+brew tap Homebrew/bundle
+
+#------------------------------------------------------------------------------
+
+echo -e "\n\nbrowsers\n"
+brew bundle -v --file=brewfiles/browsers
+
+echo -e "\n\nprogramming languages\n"
+brew bundle -v --file=brewfiles/languages
+
+echo -e "\n\neditors and general dev\n"
+brew bundle -v --file=brewfiles/editors
+
+echo -e "\n\nenvironment and shell\n"
+brew bundle -v --file=brewfiles/environment
+
+echo -e "\n\ndatabases\n"
+brew bundle -v --file=brewfiles/databases
+
+echo -e "\n\nmisc\n"
+brew bundle -v --file=brewfiles/misc
+
