e4d63cda4564a1cf7af2623e7378c138c04c026a
Richard Dowden <rd@richarddowden.com>
2016-01-12 19:34:39 +0000

    Install the very basics

--- fresh.sh
+++ fresh.sh
@@ -9,3 +9,28 @@ fi
 
 # Update homebrew recipes
 brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+# update the $PATH in your ~/.bash_profile in order to use these tools over their Mac counterparts
+$PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+
+binaries=(
+  node
+  tree
+  ack
+  git
+)
+
+brew cleanup
