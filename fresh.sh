22fbf448186ca29e1b4abe11afbb3b5a788e93f8
Richard Dowden <rd@richarddowden.com>
2017-01-05 09:20:51 +0000

    Don’t need coreutils or updated bash

--- fresh.sh
+++ fresh.sh
@@ -32,34 +32,6 @@ brew update
 
 #------------------------------------------------------------------------------
 
-echo -e "\n\ncoreutils\n"
-
-# Install GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
-
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
-
-# Install Bash 4
-brew install bash
-
-# Install more recent versions of some OS X tools
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
-
-# update the $PATH in your ~/.bash_profile in order to use these tools over their Mac counterparts
-$PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
-
-binaries=(
-  tree
-  ack
-  git
-)
-
-brew cleanup
-
-#------------------------------------------------------------------------------
-
 echo -e "\n\nhomebrew-bundle\n"
 
 brew tap Homebrew/bundle
