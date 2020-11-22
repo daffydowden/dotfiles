df853a03fc32bcc7cdb35eb1abe0d3492b9e79bb
Richard Dowden <rd@richarddowden.com>
2020-11-22 17:52:21 +0000

    Install golang, change font

--- doom.d/config.el
+++ doom.d/config.el
@@ -22,8 +22,10 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 14))
+;; (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14 :weight 'regular)
+;;       doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 14))
+(setq doom-font (font-spec :family "MesloLGL Nerd Font" :size 14 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "MesloLGL Nerd Font" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
