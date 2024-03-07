b45d24ce39a3520e1d4ec7896d5a777e2fb0b01d
Richard Dowden <rd@richarddowden.com>
2024-03-07 17:14:38 +0000

    I have no real idea what I'm doing here

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -3,8 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # controls system preferences and settings
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # everything
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # manages config links into home directory
+    #home-manager.url = "github:nix-community/home-manager/master";
+    #home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
@@ -16,12 +26,13 @@
         [
           pkgs.neovim
           pkgs.eza
+          pkgs.coreutils
         ];
+
+      environment.darwinConfig = "$HOME/projects/dotfiles";
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
@@ -32,8 +43,6 @@
       environment.shells = [ pkgs.bash pkgs.zsh pkgs.fish ];
       environment.loginShell = pkgs.fish;
 
-      environment.darwinConfig = "$HOME/projects/dotfiles";
-
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
@@ -43,6 +52,27 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "x86_64-darwin";
+
+      system.keyboard.enableKeyMapping = true;
+      system.keyboard.remapCapsLockToControl = true;
+
+      #fonts.fontsDir.enable = false; # DANGER - setting to true will delete other fonts
+      #fonts.fonts = [ pkgs.powerline-fonts (pkgs.nerd-fonts.override { fonts = [ "Meslo" "FiraCode"] }; )];
+      # nerdfonts.override { withFont = "FiraCode"; })
+      #fonts.fonts = [ (pkgs.nerd-fonts.override { fonts = [ "Meslo" ] }) ];
+
+      system.defaults.finder.AppleShowAllFiles = true;
+      system.defaults.finder.AppleShowAllExtensions = true;
+      system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+      system.defaults.NSGlobalDomain.AppleInterfaceStyleSwitchesAutomatically = true;
+      system.defaults.NSGlobalDomain.InitialKeyRepeat = 14;
+      system.defaults.NSGlobalDomain.KeyRepeat = 1;
+      system.defaults.dock.autohide = true;
+
+      system.defaults.menuExtraClock.Show24Hour = true;
+      system.defaults.menuExtraClock.ShowDate = 0;
+      system.defaults.menuExtraClock.ShowDayOfMonth = true;
+      system.defaults.menuExtraClock.ShowDayOfWeek = true;
     };
   in
   {
