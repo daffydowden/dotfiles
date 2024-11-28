c2781f346cf9b1ba100fae2bb5737c7f1bcc21f5
Richard Dowden <rd@richarddowden.com>
2024-11-28 18:57:46 +0000

    Manage neovim, kitty, bat, starship, tmux and k9s

---
+++ private_dot_config/nvim/lua/plugins/conjure.lua
@@ -0,0 +1,41 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+-- if true then return {} end
+
+-- every spec file under config.plugins will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+    "Olical/conjure",
+    ft = { "clojure", "fennel", "python" }, -- etc
+    -- [Optional] cmp-conjure for cmp
+    dependencies = {
+        {
+            "PaterJason/cmp-conjure",
+            config = function()
+                local cmp = require("cmp")
+                local config = cmp.get_config()
+                table.insert(config.sources, {
+                    name = "buffer",
+                    option = {
+                        sources = {
+                            { name = "conjure" },
+                        },
+                    },
+                })
+                cmp.setup(config)
+            end,
+        },
+    },
+    config = function(_, opts)
+        require("conjure.main").main()
+        require("conjure.mapping")["on-filetype"]()
+    end,
+    init = function()
+	       -- Set configuration options here
+        vim.g["conjure#debug"] = true
+    end,
+}
