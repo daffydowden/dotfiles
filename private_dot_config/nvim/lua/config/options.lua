bf4dbc5a8b0ee9c17f2a9a5572db9ba45707769f
Richard Dowden <rd@richarddowden.com>
2025-02-09 22:14:28 +0000

    Set some more sensible things

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,11 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- vim.g.maplocalleader = ","
 
-opts = function(_, dashboard)
-  local button = dashboard.button("p", " " .. " Projects", ":Telescope projects <CR>")
-  button.opts.hl = "AlphaButtons"
-  button.opts.hl_shortcut = "AlphaShortcut"
-  table.insert(dashboard.section.buttons.val, 4, button)
-end
+vim.g.lazyvim_picker = "snacks"
+
+-- Not a fan of autoformatting
+vim.g.autoformat = false
+
+--
+vim.g.maplocalleader = ","
+
+local opt = vim.opt
+opt.relativenumber = false
+
+-- Proper spelling ol' boy
+opt.spelllang = { "en_gb" }
