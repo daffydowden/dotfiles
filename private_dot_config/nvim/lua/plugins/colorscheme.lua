d64018b07ea71d7d3c47d62c4cbb1996ee51c47d
Richard Dowden <rd@richarddowden.com>
2025-02-28 19:56:04 +0000

    Change border colours

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,27 @@ return {
   { "sainnhe/sonokai" },
   { "ellisonleao/gruvbox.nvim" },
   { "shaunsingh/nord.nvim" },
-  { "EdenEast/nightfox.nvim" },
+  {
+    "EdenEast/nightfox.nvim",
+    priority = 1000, -- Make sure it loads first
+    opts = {
+      options = {
+        -- Optional: Change other nightfox options if desired
+        dim_inactive = true,
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          functions = "italic,bold",
+        },
+      },
+      groups = {
+        duskfox = {
+          -- Change window split color in duskfox
+          WinSeparator = { fg = "#555180" }, -- Using a purple from duskfox palette
+          VertSplit = { link = "WinSeparator" }, -- For compatibility
+        },
+      },
+    },
+  },
   { "rose-pine/neovim", name = "rose-pine" },
 }
